2c0d06adf7b6d7e416c9307dc36511c4ac57bb96
nowfelsawda <142909391+nowfelsawda@users.noreply.github.com>
2024-01-17 19:45:29 +0600

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["", "Web developer", "Graphic Designer", "Content Developer", "SEO Expert"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
